1f645120abe71937ee683c79bf3f979d6a84d033
elvolt <sidelevcs@gmail.com>
2020-09-02 14:56:36 +0300

    9.1. Рассказать и продемонстрировать как переопределяют метод hashCode [#344874]

--- chapter_001/src/main/java/ru/job4j/collection/User.java
+++ chapter_001/src/main/java/ru/job4j/collection/User.java
@@ -29,15 +29,22 @@ public class User {
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, children, birthday);
+        int result = name.hashCode();
+        result = 31 * result * Integer.hashCode(children);
+        result = 31 * result * birthday.hashCode();
+        return result;
     }
 
     public static void main(String[] args) {
         User user1 = new User("Ivan", 3, new GregorianCalendar(1974, Calendar.JANUARY, 15));
         User user2 = new User("Ivan", 3, new GregorianCalendar(1974, Calendar.JANUARY, 15));
+        User user3 = new User("Ivan", 3, new GregorianCalendar(1975, Calendar.JANUARY, 15));
+        User user4 = new User("Ivan", 2, new GregorianCalendar(1975, Calendar.JANUARY, 15));
         Set<User> map = new HashSet<>();
         map.add(user1);
         map.add(user2);
+        map.add(user3);
+        map.add(user4);
         System.out.println(map);
     }
 }
