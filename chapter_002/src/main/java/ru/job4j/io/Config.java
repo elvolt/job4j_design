bc5f7d4c48f943ba5bb5c79f62191aaa3792575b
elvolt <sidelevcs@gmail.com>
2020-09-09 21:34:36 +0300

    1. Читаем файл конфигурации [#344891]

---
+++ chapter_002/src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,44 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(path))) {
+            read.lines()
+                    .filter(line -> !line.startsWith("#"))
+                    .forEach(line -> {
+                        String[] pair = line.split("=");
+                        values.put(pair[0], pair[1]);
+                    });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("name"),
+                is("Ivan Ivanov")
+        );
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "./data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("key1"),
+                is("value1")
+        );
+        assertThat(
+                config.value("key2"),
+                is("value2")
+        );
+    }
+}
