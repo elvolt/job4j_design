6475fdc3d8c3deb34ee531eb72255f00e38a773f
elvolt <sidelevcs@gmail.com>
2020-09-23 15:21:16 +0300

    5.2. Архивировать проект [#344893]

--- chapter_002/src/main/java/ru/job4j/io/Pack.java
+++ chapter_002/src/main/java/ru/job4j/io/Pack.java
@@ -1,23 +1,29 @@
 package ru.job4j.io;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Pack {
+    private static List<File> searchFiles(Path root, List<String> excludeExtensions)
+            throws IOException {
+        return Search.exclude(root, excludeExtensions)
+                .stream()
+                .map(Path::toFile)
+                .collect(Collectors.toList());
+    }
 
     public static void main(String[] args) {
         Zip zip = new Zip();
         ArgZip keys = new ArgZip(args);
         Path root = Paths.get(keys.directory());
         try {
-            List<File> files = Search.exclude(root, keys.exclude())
-                    .stream()
-                    .map(Path::toFile)
-                    .collect(Collectors.toList());
-            zip.packFiles(files, new File(keys.output()), root.toFile());
+            zip.packFiles(
+                    searchFiles(root, keys.exclude()), new File(keys.output()), root.toFile()
+            );
         } catch (Exception e) {
             e.printStackTrace();
         }
