7048c923d45f570a1a33cd2bb76b96bcc02efe5c
elvolt <sidelevcs@gmail.com>
2020-09-25 14:00:05 +0300

    6. Консольный чат. [#344894] apply dispatch pattern

--- chapter_002/src/main/java/ru/job4j/io/chat/Chat.java
+++ chapter_002/src/main/java/ru/job4j/io/chat/Chat.java
@@ -1,30 +1,62 @@
 package ru.job4j.io.chat;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class Chat {
     private static final String STOP_WORD = "Стоп";
-    private static final String CONTINUE_WORD = "Продолжить";
-    private static final String END_WORD = "Закончить";
+    private static final String RESUME_WORD = "Продолжить";
+    private static final String FINISH_WORD = "Закончить";
     private File phrasesSource = new File(".\\chapter_002\\data\\phrases_source.txt");
     private File chatLog = new File(".\\chapter_002\\chat_log.txt");
+    private boolean botActive = true;
+    private boolean chatActive = true;
+    private final Map<String, Consumer<Chat>> dispatch = new HashMap<>();
 
     public Chat() {
+        initDispatch();
     }
 
     public Chat(File phrasesSource, File chatLog) {
+        this();
         this.phrasesSource = phrasesSource;
         this.chatLog = chatLog;
     }
 
+    private Consumer<Chat> stop() {
+        return chat -> chat.botActive = false;
+    }
+
+    private Consumer<Chat> resume() {
+        return chat -> chat.botActive = true;
+    }
+
+    private Consumer<Chat> finish() {
+        return chat -> {
+            chat.botActive = false;
+            chat.chatActive = false;
+        };
+    }
+
+    public void loadToDispatch(String word, Consumer<Chat> handle) {
+        dispatch.put(word, handle);
+    }
+
+    private void initDispatch() {
+        loadToDispatch(STOP_WORD, stop());
+        loadToDispatch(RESUME_WORD, resume());
+        loadToDispatch(FINISH_WORD, finish());
+    }
+
+    public void checkUserText(String userText) {
+        if (dispatch.containsKey(userText)) {
+            dispatch.get(userText).accept(this);
+        }
+    }
+
     public void chat() {
-        boolean botActive = true;
         List<String> log = new ArrayList<>();
         Answer answer = new Answer(phrasesSource);
         Scanner scanner = new Scanner(System.in);
@@ -33,20 +65,18 @@ public class Chat {
         do {
             userText = scanner.nextLine();
             log.add("User: " + userText);
-            if (userText.equals(STOP_WORD)) {
-                botActive = false;
-                continue;
-            }
-            if (userText.equals(CONTINUE_WORD)) {
-                botActive = true;
-            }
-            if (botActive && !userText.equals(END_WORD)) {
+            checkUserText(userText);
+            if (botActive) {
                 String currentAnswer = answer.generateAnswer();
                 System.out.println(currentAnswer);
                 log.add("Bot: " + currentAnswer);
             }
-        } while (!userText.equals(END_WORD));
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(chatLog))) {
+        } while (chatActive);
+        try (PrintWriter writer = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(chatLog)
+                )
+        )) {
             for (String logItem : log) {
                 writer.write(logItem + System.lineSeparator());
             }
