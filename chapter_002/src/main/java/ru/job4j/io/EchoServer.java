d9020778139832bb8923cb68d26d1782cae52de2
elvolt <sidelevcs@gmail.com>
2020-09-25 21:13:28 +0300

    4. Slf4j - вывод exception. [#344880]

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -1,5 +1,8 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,30 +13,36 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(9000)) {
-            while (true) {
-                Socket socket = server.accept();
-                try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(
-                             new InputStreamReader(socket.getInputStream()))) {
-                    String message = "";
-                    String str;
-                    while (!(str = in.readLine()).isEmpty()) {
-                        System.out.println(str);
-                        Pattern pattern = Pattern.compile("\\?msg=\\S+");
-                        Matcher matcher = pattern.matcher(str);
-                        while (matcher.find()) {
-                            message = matcher.group().split("msg=")[1].trim();
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
+
+    public static void main(String[] args) {
+        try {
+            try (ServerSocket server = new ServerSocket(9000)) {
+                while (true) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream();
+                         BufferedReader in = new BufferedReader(
+                                 new InputStreamReader(socket.getInputStream()))) {
+                        String message = "";
+                        String str;
+                        while (!(str = in.readLine()).isEmpty()) {
+                            System.out.println(str);
+                            Pattern pattern = Pattern.compile("\\?msg=\\S+");
+                            Matcher matcher = pattern.matcher(str);
+                            while (matcher.find()) {
+                                message = matcher.group().split("msg=")[1].trim();
+                            }
                         }
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        if (message.equals("Exit")) {
+                            break;
+                        }
+                        out.write(message.getBytes());
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    if (message.equals("Exit")) {
-                        break;
-                    }
-                    out.write(message.getBytes());
                 }
             }
+        } catch (IOException e) {
+            LOG.error("Exception in main", e);
         }
     }
 }
