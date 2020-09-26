5619219f0e4c6ebffd3735514b36095b638b125a
elvolt <sidelevcs@gmail.com>
2020-09-26 21:57:40 +0300

    5. Преобразование JSON в POJO. JsonObject [#344901]

--- chapter_002/src/main/java/ru/job4j/io/serialization/Car.java
+++ chapter_002/src/main/java/ru/job4j/io/serialization/Car.java
@@ -2,6 +2,8 @@ package ru.job4j.io.serialization;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -18,6 +20,26 @@ public class Car {
         this.accident = accident;
         this.engine = engine;
         this.owners = owners;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public boolean isAccident() {
+        return accident;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public Person[] getOwners() {
+        return owners;
     }
 
     @Override
@@ -42,5 +64,17 @@ public class Car {
         System.out.println(carJson);
         final Car carMod = gson.fromJson(carJson, Car.class);
         System.out.println(carMod);
+        System.out.println();
+        System.out.println();
+        final JSONObject jsonCar = new JSONObject();
+        jsonCar.put("mark", bmw.getMark());
+        jsonCar.put("year", bmw.getYear());
+        jsonCar.put("accident", bmw.isAccident());
+        jsonCar.put("engine", new JSONObject("{\"type\":\"V8\",\"year\":2017}"));
+        JSONArray owners = new JSONArray();
+        owners.put(new JSONObject("{\"name\":\"Fill\",\"age\":32}"));
+        owners.put(new JSONObject("{\"name\":\"Mary\",\"age\":23}"));
+        jsonCar.put("owners", owners);
+        System.out.println(jsonCar.toString());
     }
 }
