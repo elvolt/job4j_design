3b5dc2fd524bb51d98b01acfa71b578ed6bf19bc
elvolt <sidelevcs@gmail.com>
2021-06-04 14:16:28 +0300

    refactor: 1. Экзамен. Ввод-вывод. [#571 #163315]. Example without java.nio

--- chapter_002/src/main/java/ru/job4j/io/puzzle/Shell.java
+++ chapter_002/src/main/java/ru/job4j/io/puzzle/Shell.java
@@ -1,23 +1,30 @@
 package ru.job4j.io.puzzle;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class Shell {
-    private Path workingDirectory = Paths.get("/");
+    private String workingDirectory = "/";
+
+    private String normalize(String path) {
+        return path
+                // replace "./" in path
+                .replaceAll("/\\./", "")
+                // replace ".." in path
+                .replaceAll("/\\w*/\\.\\.", "/")
+                // replace multiple "//" in path
+                .replaceAll("/{2,}", "/")
+                // remove last "/" if path contains it
+                .replaceAll("\\b/$", "");
+    }
 
     public Shell cd(String path) {
-        String normalizedPath = path.replaceAll("/{2,}", "");
-        Path filepath = Paths.get(normalizedPath);
-        if (filepath.isAbsolute()) {
-            workingDirectory = filepath.normalize();
+        if (path.startsWith("/")) {
+            workingDirectory = normalize(path);
         } else {
-            workingDirectory = workingDirectory.resolve(filepath).normalize();
+            workingDirectory = normalize(workingDirectory + "/" + path);
         }
         return this;
     }
 
     public String path() {
-        return workingDirectory.toString();
+        return workingDirectory;
     }
 }
